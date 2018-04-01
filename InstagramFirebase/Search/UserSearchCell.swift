0982df21b3bb14d625aa8469c69e2ce0097ca9f6
Amith Dubbasi <amithdubbasi@UCITMac.local>
2018-03-31 23:50:29 -0400

    UI changes, in User Profile collectionView

--- InstagramFirebase/Search/UserSearchCell.swift
+++ InstagramFirebase/Search/UserSearchCell.swift
@@ -22,7 +22,7 @@ class UserSearchCell: UICollectionViewCell {
     
     let profileImageView: CustomImageView = {
         let iv = CustomImageView()
-        iv.backgroundColor = .red
+        iv.backgroundColor = .lightGray
         iv.contentMode = .scaleAspectFill
         iv.clipsToBounds = true
         return iv
