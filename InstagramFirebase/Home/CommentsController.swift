342093a98ba039b12c3af4a8ea9c4a19f094de59
Amith Dubbasi <amithdubbasi@UCITMac.local>
2018-04-11 13:16:49 -0400

    Custom Delegation

---
+++ InstagramFirebase/Home/CommentsController.swift
@@ -0,0 +1,18 @@
+//
+//  CommentsController.swift
+//  InstagramFirebase
+//
+//  Created by Amith Dubbasi on 4/11/18.
+//  Copyright © 2018 iDiscover. All rights reserved.
+//
+
+import UIKit
+
+class CommentsController : UICollectionViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .yellow
+    }
+}
